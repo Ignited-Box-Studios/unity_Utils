ddf92b8aae3b958022a0acaf50b2295f3ebbc1e1
Eivlisskiv <gautma13@gmail.com>
2024-10-31 21:16:58 -0400

    writable importer concept

---
+++ Serialization/WritableAssetFile/WritableImporter.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+using UnityEditor.AssetImporters;
+
+namespace UnityUtils.Serialization.WritableAssetFile
+{
+	public class WritableImporter : ScriptedImporter
+	{
+		public override void OnImportAsset(AssetImportContext ctx)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
+#endif
