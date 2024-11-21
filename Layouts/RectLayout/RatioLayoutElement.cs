8243c111c7ad9f37adfbc6cd6fe5a1475866b411
Eivlisskiv <gautma13@gmail.com>
2024-11-21 00:13:32 -0500

    added rect layout option to maintain non axis size;

--- Layouts/RectLayout/RatioLayoutElement.cs
+++ Layouts/RectLayout/RatioLayoutElement.cs
@@ -6,6 +6,9 @@ namespace UnityUtils.Layouts.RectLayout
 	public class RatioLayoutElement : RectLayoutElement
 	{
 		[SerializeField] private float ratio;
+		[SerializeField] private bool useCurrentSize;
+
+		private Rect Rect => (transform as RectTransform).rect;
 
 		protected override void OnSetEnabled(bool enabled)
 		{
@@ -15,17 +18,18 @@ namespace UnityUtils.Layouts.RectLayout
 		protected override Rect GetHorizontalRectLayout(Rect offset, RectTransform parent)
 		{
 			Rect parentRect = parent.rect;
-			return new Rect(
-					offset.xMax, 0,
-					ratio.GetWidthFromHeight(parentRect.height), parentRect.height
+			return new Rect(offset.xMax, 0,
+					ratio.GetWidthFromHeight(parentRect.height), 
+					useCurrentSize ? Rect.height : parentRect.height
 				);
 		}
 		protected override Rect GetVerticalRectLayout(Rect offset, RectTransform parent)
 		{
 			Rect parentRect = parent.rect;
-			return new Rect(
-					0, offset.yMax,
-					parentRect.width, ratio.GetHeightFromWidth(parentRect.width)
+			
+			return new Rect(0, offset.yMax,
+					useCurrentSize ? Rect.width : parentRect.width,
+					ratio.GetHeightFromWidth(parentRect.width)
 				);
 		}
 	}
